5b6e5111ef13dbaea9cd2b71f1295cc839965003
developerX <x@developerx.io>
2019-02-18 21:32:24 -0800

    added in multiple features to be analyzed

--- index.js
+++ index.js
@@ -7,21 +7,47 @@ const outputs = require('./data/oneByOne.json');
 //The K-nearest Neighbor Algorithm in practice
 function knn(data, point, k) {
   return _.chain(data)
-    .map(row => [distance(row[0], point), row[3]])
+    // loop through every data point and do a calculation
+    .map(row => {
+      return [
+        distance(
+          _.initial(row),
+          point
+        ),
+        _.last(row)
+      ]
+    })
+    // sort by the distance
     .sortBy(row => row[0])
+    // return the data by the amount of K
     .slice(0, k)
+    // Organize by most common outcome
     .countBy(row => row[1])
+    // Turn them back into an array
     .toPairs()
+    // Sort them in ascending order
     .sortBy(row => row[1])
+    // return the last array (most common output)
     .last()
+    // return the value of most likely return
     .first()
+    //convert back into a number
     .parseInt()
+    // return the chain
     .value()
 }
 
 // Calculates the absolute distance between 2 points
 function distance(pointA, pointB) {
-  return Math.abs(pointA - pointB);
+  // multiple features
+  return _.chain(pointA)
+    .zip(pointB)
+    .map(([a, b]) => (a - b) ** 2)
+    .sum()
+    .value() ** 0.5;
+
+  // single feature based off point
+  // return Math.abs(pointA - pointB);
 }
 
 //Splits data into a test set and a training set
@@ -32,8 +58,22 @@ function splitDataset(data, testCount) {
   return [testSet, trainingSet];
 }
 
+function normalizeData(data, featureCount) {
+  const clonedData = _.cloneDeep(data);
+  for (let i = 0; i < featureCount; i++) {
+    const column = clonedData.map(row => row[i]);
+    const min = _.min(column);
+    const max = _.max(column);
+    for (let j = 0; j < clonedData.length; j++) {
+      clonedData[j][i] = (clonedData[j][i] - min) / (max - min);
+    }
+  }
+
+  return clonedData;
+}
+
 //Determines how big of a test set size
-const testSetSize = 500;
+const testSetSize = 200;
 
 // Array destructuring into a test set and a training set
 const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
@@ -42,12 +82,15 @@ const [testSet, trainingSet] = splitDataset(outputs, testSetSize);
 _.range(1, 30).forEach(k => {
   // runs KNN on each range value and tests for accuracy
   const accuracy = _.chain(testSet)
-    .filter(testPoint => knn(trainingSet, testPoint[0], k) === testPoint[3])
+    .filter(testPoint => knn(
+      normalizeData(trainingSet, 3),
+      _.initial(testPoint),
+      k) === testPoint[3])
     .size()
     .divide(testSetSize)
     .multiply(100)
     .value()
 
   //console logs the output
-  console.log('Accuracy:', accuracy, "K is equal to", k);
+  console.log('Accuracy:', accuracy, "% K is equal to", k);
 });
